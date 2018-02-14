538392a525629f9639acd3b205c39aeaeacd26a7
Armaan Sethi <armaan.sethi@gmail.com>
2018-02-14 01:25:25 -0500

    fix errors that occur when user does not exist

--- fb-parse.py
+++ fb-parse.py
@@ -23,7 +23,7 @@ def parse_file(f):
     # print("Ppl", people)
 
     title = soup.find("title").contents[0][18:] #name of conversation
-    print("TITLE", title)
+    # print("TITLE", title)
 
     # print("Opened messages.htm")
     soup = soup.find_all("div", class_="message")
@@ -34,40 +34,44 @@ def parse_file(f):
 
     times = []
     dates = []
-    message = []
+    texts = []
     user = []
 
     for msg in tqdm(soup):
         (date, time) = msg.find_all("span", class_="meta")[0].contents[0].split(" at ")
-
-        usr = msg.find_all("span", class_="user")[0].contents[0]
-        usr = people[0][1:]
-
+        try:
+            usr = msg.find_all("span", class_="user")[0].contents[0]
+            # user.append(msg.find_all("span", class_="user")[0].contents[0])
+        except:
+            if(not group):
+                usr = people[0][1:]
+            else:
+                usr = ""
         try:
             text = msg.find_next_sibling("p").contents[0]
         except:
             text = ""
-            #Not sure what to do here
 
         dates.append(date)
         times.append(time)
-        message.append(text)
+        texts.append(text)
         user.append(usr)
 
 
     """OUTPUT"""
     # print(len(dates))
     # print(len(user))
-    # print(len(message))
+    # print(len(texts))
     num_messages = len(dates) #number of messages
-
     """Output all messages in order"""
     # for i in range(num_messages):
-    #     print(user[num_messages-1-i], '\t', message[num_messages-1-i],'\t', dates[num_messages-1-i])
+    #     print(user[num_messages-1-i], '\t', texts[num_messages-1-i],'\t', dates[num_messages-1-i])
+
 
 def find_html_filenames( path_to_dir, suffix=".html" ):
     filenames = os.listdir(path_to_dir)
     return [ filename for filename in filenames if filename.endswith( suffix ) ]
+
 
 def parse_folder():
     os.chdir("facebook/messages")
@@ -76,10 +80,9 @@ def parse_folder():
 
     for f in tqdm(files):
         parse_file(f)
-        print()
 
 
-parse_folder()
+# parse_folder()
 # parse_file("facebook/messages/356.html") #has no user but has names
 # parse_file("facebook/messages/421.html") #has no name but has messages
-# parse_file("facebook/messages/7.html") #group chat
+parse_file("facebook/messages/7.html") #group chat
